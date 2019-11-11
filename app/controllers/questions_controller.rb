dde98c460e4a9e920556f648157a3965d85c3f7a
Desiree Buelter <desiree.buelter@web.de>
2019-11-11 15:23:50 +0100

    game

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+   @question = params[:question]
+   if @question == 'i am going to work right now!'
+     @answer = 'great bye bye'
+   elsif @question[-1] == '?'
+     @answer = 'Silly question, get dressed and go to work!'
+   else
+     @answer = 'I dont care, get dressed and go to work!'
+   end
+  end
+end
